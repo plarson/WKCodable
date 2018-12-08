3614f7739fd7de9fbabe857a2b67175fa41027b6
Phil Larson <phil.larson@icloud.com>
2018-12-08 14:50:01 -0800

    Add littleEndian tests

--- Tests/WKCodableTests/WKBCodableLittleEndianTests.swift
+++ Tests/WKCodableTests/WKBCodableLittleEndianTests.swift
@@ -1,36 +1,36 @@
 import XCTest
 @testable import WKCodable
 
-class WKBCodableTests: XCTestCase {
+class WKBCodableLittleEndianTests: XCTestCase {
     
     var encoder: WKBEncoder!
     var decoder: WKBDecoder!
-
+    
     override func setUp() {
-        encoder = WKBEncoder()
+        encoder = WKBEncoder(byteOrder: .littleEndian)
         decoder = WKBDecoder()
     }
     
     func testPoint2D() throws {
         let value = WKBPoint(vector: [1,2])
         let data = try encoder.encode(value)
-        XCTAssertEqual("0020000001000003e83ff00000000000004000000000000000", data.hexEncodedString())
+        XCTAssertEqual("0101000020e8030000000000000000f03f0000000000000040", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBPoint
         XCTAssertEqual(value, value2)
     }
-        
+    
     func testPoint3D() throws {
         let value = WKBPoint(vector: [1,2,3])
         let data = try encoder.encode(value)
-        XCTAssertEqual("00a0000001000003e83ff000000000000040000000000000004008000000000000", data.hexEncodedString())
+        XCTAssertEqual("01010000a0e8030000000000000000f03f00000000000000400000000000000840", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBPoint
         XCTAssertEqual(value, value2)
     }
-
+    
     func testPoint4D() throws {
         let value = WKBPoint(vector: [1,2,3,4])
         let data = try encoder.encode(value)
-        XCTAssertEqual("00e0000001000003e83ff0000000000000400000000000000040080000000000004010000000000000", data.hexEncodedString())
+        XCTAssertEqual("01010000e0e8030000000000000000f03f000000000000004000000000000008400000000000001040", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBPoint
         XCTAssertEqual(value, value2)
     }
@@ -38,37 +38,37 @@ class WKBCodableTests: XCTestCase {
     func testLineStringEmpty() throws {
         let value = WKBLineString(points: [])
         let data = try encoder.encode(value)
-        XCTAssertEqual("0020000002000003e8", data.hexEncodedString())
+        XCTAssertEqual("0102000020e8030000", data.hexEncodedString())
     }
-
+    
     func testLineString2D() throws {
         let value = WKBLineString(points: [ WKBPoint(vector: [1,2]), WKBPoint(vector: [2,3]) ])
         let data = try encoder.encode(value)
-        XCTAssertEqual("0020000002000003e8000000023ff0000000000000400000000000000040000000000000004008000000000000", data.hexEncodedString())
+        XCTAssertEqual("0102000020e803000002000000000000000000f03f000000000000004000000000000000400000000000000840", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBLineString
         XCTAssertEqual(value, value2)
     }
-
+    
     func testLineString3D() throws {
         let value = WKBLineString(points: [ WKBPoint(vector: [1,2,3]), WKBPoint(vector: [4,5,6]) ])
         let data = try encoder.encode(value)
-        XCTAssertEqual("00a0000002000003e8000000023ff000000000000040000000000000004008000000000000401000000000000040140000000000004018000000000000", data.hexEncodedString())
+        XCTAssertEqual("01020000a0e803000002000000000000000000f03f00000000000000400000000000000840000000000000104000000000000014400000000000001840", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBLineString
         XCTAssertEqual(value, value2)
     }
-
+    
     func testLineString4D() throws {
         let value = WKBLineString(points: [ WKBPoint(vector: [1,2,3,4]), WKBPoint(vector: [5,6,7,8]) ])
         let data = try encoder.encode(value)
-        XCTAssertEqual("00e0000002000003e8000000023ff000000000000040000000000000004008000000000000401000000000000040140000000000004018000000000000401c0000000000004020000000000000", data.hexEncodedString())
+        XCTAssertEqual("01020000e0e803000002000000000000000000f03f000000000000004000000000000008400000000000001040000000000000144000000000000018400000000000001c400000000000002040", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBLineString
         XCTAssertEqual(value, value2)
     }
-
+    
     func testPolygonEmpty() throws {
         let value = WKBPolygon(exteriorRing: WKBLineString(points: []))
         let data = try encoder.encode(value)
-        XCTAssertEqual("0020000003000003e800000000", data.hexEncodedString())
+        XCTAssertEqual("0103000020e803000000000000", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBPolygon
         XCTAssertEqual(value, value2)
     }
@@ -82,7 +82,7 @@ class WKBCodableTests: XCTestCase {
             ])
         let value = WKBPolygon(exteriorRing: lineString)
         let data = try encoder.encode(value)
-        XCTAssertEqual("0020000003000003e800000001000000043ff0000000000000400000000000000040080000000000004010000000000000401800000000000040140000000000003ff00000000000004000000000000000", data.hexEncodedString())
+        XCTAssertEqual("0103000020e80300000100000004000000000000000000f03f00000000000000400000000000000840000000000000104000000000000018400000000000001440000000000000f03f0000000000000040", data.hexEncodedString())
         let value2 = try decoder.decode(from: data) as! WKBPolygon
         XCTAssertEqual(value, value2)
     }
--- Tests/WKCodableTests/XCTestManifests.swift
+++ Tests/WKCodableTests/XCTestManifests.swift
@@ -1,5 +1,19 @@
 import XCTest
 
+extension WKBCodableLittleEndianTests {
+    static let __allTests = [
+        ("testLineString2D", testLineString2D),
+        ("testLineString3D", testLineString3D),
+        ("testLineString4D", testLineString4D),
+        ("testLineStringEmpty", testLineStringEmpty),
+        ("testPoint2D", testPoint2D),
+        ("testPoint3D", testPoint3D),
+        ("testPoint4D", testPoint4D),
+        ("testPolygon", testPolygon),
+        ("testPolygonEmpty", testPolygonEmpty),
+    ]
+}
+
 extension WKBCodableTests {
     static let __allTests = [
         ("testLineString2D", testLineString2D),
@@ -17,6 +31,7 @@ extension WKBCodableTests {
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
+        testCase(WKBCodableLittleEndianTests.__allTests),
         testCase(WKBCodableTests.__allTests),
     ]
 }
