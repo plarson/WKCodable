d765c0e1ee39d7bbd0c2d4dda0f0e931fed2eee7
Phil Larson <phil.larson@icloud.com>
2018-12-08 15:57:52 -0800

    swift test --generate-linuxmain

--- Tests/WKCodableTests/XCTestManifests.swift
+++ Tests/WKCodableTests/XCTestManifests.swift
@@ -2,10 +2,17 @@ import XCTest
 
 extension WKBCodableLittleEndianTests {
     static let __allTests = [
+        ("testGeometryCollectionEmpty", testGeometryCollectionEmpty),
         ("testLineString2D", testLineString2D),
         ("testLineString3D", testLineString3D),
         ("testLineString4D", testLineString4D),
         ("testLineStringEmpty", testLineStringEmpty),
+        ("testMultiLineString", testMultiLineString),
+        ("testMultiLineStringEmpty", testMultiLineStringEmpty),
+        ("testMultiPoint", testMultiPoint),
+        ("testMultiPointEmpty", testMultiPointEmpty),
+        ("testMultiPolygonEmpty", testMultiPolygonEmpty),
+        ("testMultiPolygonString", testMultiPolygonString),
         ("testPoint2D", testPoint2D),
         ("testPoint3D", testPoint3D),
         ("testPoint4D", testPoint4D),
@@ -16,10 +23,17 @@ extension WKBCodableLittleEndianTests {
 
 extension WKBCodableTests {
     static let __allTests = [
+        ("testGeometryCollectionEmpty", testGeometryCollectionEmpty),
         ("testLineString2D", testLineString2D),
         ("testLineString3D", testLineString3D),
         ("testLineString4D", testLineString4D),
         ("testLineStringEmpty", testLineStringEmpty),
+        ("testMultiLineString", testMultiLineString),
+        ("testMultiLineStringEmpty", testMultiLineStringEmpty),
+        ("testMultiPoint", testMultiPoint),
+        ("testMultiPointEmpty", testMultiPointEmpty),
+        ("testMultiPolygonEmpty", testMultiPolygonEmpty),
+        ("testMultiPolygonString", testMultiPolygonString),
         ("testPoint2D", testPoint2D),
         ("testPoint3D", testPoint3D),
         ("testPoint4D", testPoint4D),
